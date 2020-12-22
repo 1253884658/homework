9fc9b91539f48c5116f6b121356c22d9bec0afec
1253884658 <1253884658@qq.com>
2020-12-22 20:19:35 +0800

    提交作业

---
+++ bookmanager/book/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "book/index.html")
